7a63390edc357d30609e0cdf2cde067e93e7551b
yiuked <dev@example.com>
2023-03-05 21:52:45 +0800

    fix:修正超时问题

--- apis/internet/text.go
+++ apis/internet/text.go
@@ -6,10 +6,18 @@ import (
 	"context"
 	"encoding/xml"
 	"fmt"
+	"go.mongodb.org/mongo-driver/bson"
 	"log"
 	"strings"
+	"sync"
 	"time"
 )
+
+var locks map[string]*sync.Mutex
+
+func init() {
+	locks = make(map[string]*sync.Mutex)
+}
 
 func TextHandel(ctx *MsgContext) {
 	if v, b := config.Limit[ctx.Msg.FromUserName]; b {
@@ -23,6 +31,39 @@ func TextHandel(ctx *MsgContext) {
 		}
 	} else {
 		config.Limit[ctx.Msg.FromUserName] = &config.UserLimit{Cnt: 0, LastSt: time.Now().Unix()}
+	}
+
+	if _, ok := locks[ctx.Msg.FromUserName]; !ok {
+		locks[ctx.Msg.FromUserName] = &sync.Mutex{}
+
+	}
+	// 加锁，保障只有一次请求到openai
+	locks[ctx.Msg.FromUserName].Lock()
+	defer locks[ctx.Msg.FromUserName].Unlock()
+
+	// 查询结果是否存在，存在则直接返回
+	var existsFaq Faq
+	if err := config.Mgo.Db.Collection("faq").FindOne(context.Background(), bson.M{"question": ctx.Msg.Content}).
+		Decode(&existsFaq); err == nil {
+		resp := Response{
+			ToUserName:   ctx.Msg.FromUserName,
+			FromUserName: ctx.Msg.ToUserName,
+			CreateTime:   ctx.Msg.CreateTime,
+			MsgType:      "text",
+			Content:      existsFaq.Answer,
+		}
+		res, err := xml.Marshal(resp)
+		if err != nil {
+			fmt.Println("xml.Marshal error: ", err)
+			Error(ctx.ResponseWriter)
+			return
+		}
+		log.Println("response=> ", string(res))
+
+		config.Limit[ctx.Msg.FromUserName].Cnt += 1
+		config.Limit[ctx.Msg.FromUserName].LastSt = time.Now().Unix()
+		fmt.Fprintf(ctx.ResponseWriter, string(res))
+		return
 	}
 
 	result := lib.Send(ctx.Msg.Content)
