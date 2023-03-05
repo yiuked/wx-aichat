97e9fbb7edf3b42c58d23075ff3d812ceaa731e8
yiuked <dev@example.com>
2023-03-05 23:36:39 +0800

    fix:修正超时问题

--- apis/internet/text.go
+++ apis/internet/text.go
@@ -35,7 +35,6 @@ func TextHandel(ctx *MsgContext) {
 
 	if _, ok := locks[ctx.Msg.FromUserName]; !ok {
 		locks[ctx.Msg.FromUserName] = &sync.Mutex{}
-
 	}
 	// 加锁，保障只有一次请求到openai
 	locks[ctx.Msg.FromUserName].Lock()
@@ -66,42 +65,59 @@ func TextHandel(ctx *MsgContext) {
 		return
 	}
 
-	result := lib.Send(ctx.Msg.Content)
-	if result == nil {
-		Error(ctx.ResponseWriter)
-	}
-
-	var returnMsg string
-	for _, msg := range result {
-		for _, m := range msg.Choices {
-			returnMsg = returnMsg + strings.Trim(m.Message.Content, "\n")
-		}
-	}
-
 	resp := Response{
 		ToUserName:   ctx.Msg.FromUserName,
 		FromUserName: ctx.Msg.ToUserName,
 		CreateTime:   ctx.Msg.CreateTime,
 		MsgType:      "text",
-		Content:      returnMsg,
 	}
 
-	faq := Faq{UserName: ctx.Msg.FromUserName, CreateTime: time.Now().Unix(), Question: ctx.Msg.Content, Answer: resp.Content}
-	if _, err := config.Mgo.Db.Collection("faq").InsertOne(context.Background(), &faq); err != nil {
-		log.Println("mongodb insert err,", err)
-		Error(ctx.ResponseWriter)
+	go func() {
+		result := lib.Send(ctx.Msg.Content)
+		if result == nil {
+			Error(ctx.ResponseWriter)
+		}
+		var returnMsg string
+		for _, msg := range result {
+			for _, m := range msg.Choices {
+				returnMsg = returnMsg + strings.Trim(m.Message.Content, "\n")
+			}
+		}
+		resp.Content = returnMsg
+
+		faq := Faq{UserName: ctx.Msg.FromUserName, CreateTime: time.Now().Unix(), Question: ctx.Msg.Content, Answer: resp.Content}
+		if _, err := config.Mgo.Db.Collection("faq").InsertOne(context.Background(), &faq); err != nil {
+			log.Println("mongodb insert err,", err)
+			Error(ctx.ResponseWriter)
+			return
+		}
+
+		res, err := xml.Marshal(resp)
+		if err != nil {
+			fmt.Println("xml.Marshal error: ", err)
+			Error(ctx.ResponseWriter)
+			return
+		}
+		log.Println("response=> ", string(res))
+
+		config.Limit[ctx.Msg.FromUserName].Cnt += 1
+		config.Limit[ctx.Msg.FromUserName].LastSt = time.Now().Unix()
+		fmt.Fprintf(ctx.ResponseWriter, string(res))
+	}()
+
+	select {
+	// 微信超时是3次重发都没返回则超时，每次5秒（3*5=15秒），这里设置13秒，没结果就先返回
+	case <-time.After(13 * time.Second):
+		fmt.Println("超时")
+		resp.Content = fmt.Sprintf(`<a href="weixin://bizmsgmenu?msgmenucontent=%s&msgmenuid=1">请求超时，点击重试</a>`, ctx.Msg.Content)
+		res, err := xml.Marshal(resp)
+		if err != nil {
+			fmt.Println("xml.Marshal error: ", err)
+			Error(ctx.ResponseWriter)
+			return
+		}
+		fmt.Fprintf(ctx.ResponseWriter, string(res))
 		return
 	}
 
-	res, err := xml.Marshal(resp)
-	if err != nil {
-		fmt.Println("xml.Marshal error: ", err)
-		Error(ctx.ResponseWriter)
-		return
-	}
-	log.Println("response=> ", string(res))
-
-	config.Limit[ctx.Msg.FromUserName].Cnt += 1
-	config.Limit[ctx.Msg.FromUserName].LastSt = time.Now().Unix()
-	fmt.Fprintf(ctx.ResponseWriter, string(res))
 }
