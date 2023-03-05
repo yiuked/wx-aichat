f8f11ed299dc37629ec7b35071c8d558b4332ad4
yiuked <dev@example.com>
2023-03-06 00:01:41 +0800

    fix:修正超时问题

--- apis/internet/text.go
+++ apis/internet/text.go
@@ -109,7 +109,7 @@ func TextHandel(ctx *MsgContext) {
 	// 微信超时是3次重发都没返回则超时，每次5秒（3*5=15秒），这里设置10秒，没结果就先返回
 	case <-time.After(10 * time.Second):
 		log.Println("请求超时")
-		resp.Content = fmt.Sprintf(`<a href="weixin://bizmsgmenu?msgmenucontent=%s&msgmenuid=1">请求超时，点击重试</a>`, ctx.Msg.Content)
+		resp.Content = fmt.Sprintf(`&lt;a href=&quot;weixin://bizmsgmenu?msgmenucontent=%s&amp;msgmenuid=1&quot;&gt;请求超时，点击重试&lt;/a&gt;`, ctx.Msg.Content)
 		res, err := xml.Marshal(resp)
 		if err != nil {
 			fmt.Println("xml.Marshal error: ", err)
