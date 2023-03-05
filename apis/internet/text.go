94d08c0f25a4af2535555a88b134ae35634ab4f7
yiuked <dev@example.com>
2023-03-06 00:22:33 +0800

    fix:修正超时问题

--- apis/internet/text.go
+++ apis/internet/text.go
@@ -8,6 +8,7 @@ import (
 	"fmt"
 	"go.mongodb.org/mongo-driver/bson"
 	"log"
+	"net/http"
 	"strings"
 	"sync"
 	"time"
@@ -110,7 +111,7 @@ func TextHandel(ctx *MsgContext) {
 	// 微信超时是3次重发都没返回则超时，每次5秒（3*5=15秒），这里设置10秒，没结果就先返回
 	case <-time.After(10 * time.Second):
 		log.Println("请求超时")
-		resp.Content = fmt.Sprintf(`<a href="weixin://bizmsgmenu?msgmenucontent=请求超时，点击重试&msgmenuid=1">请求超时，点击重试</a>`)
+		resp.Content = fmt.Sprintf(`<a href="weixin://bizmsgmenu?msgmenucontent=%s&msgmenuid=1">请求超时，点击重试</a>`, ctx.Msg.Content)
 		res, err := xml.Marshal(resp)
 		if err != nil {
 			fmt.Println("xml.Marshal error: ", err)
@@ -118,10 +119,8 @@ func TextHandel(ctx *MsgContext) {
 			return
 		}
 		log.Println("response=> ", string(res))
-		_, err = fmt.Fprintf(ctx.ResponseWriter, string(res))
-		if err != nil {
-			log.Println("发送失败：", err)
-		}
+		fmt.Fprintf(ctx.ResponseWriter, string(res))
+		ctx.ResponseWriter.(http.Flusher).Flush()
 		return
 	}
 
