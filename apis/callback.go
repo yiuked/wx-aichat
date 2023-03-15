706c1d0f9ff8dffb88c645fc9b3e6ed17ce9e16e
yiuked <dev@example.com>
2023-03-15 23:01:18 +0800

    fix:修正签名验证时限

--- apis/callback.go
+++ apis/callback.go
@@ -13,23 +13,25 @@ import (
 	"log"
 	"net/http"
 	"sort"
+	"strconv"
 	"strings"
+	"time"
 )
 
 func Callback(w http.ResponseWriter, r *http.Request) {
+	// 微信服务器验证
+	signature := r.URL.Query().Get("signature")
+	timestamp := r.URL.Query().Get("timestamp")
+	nonce := r.URL.Query().Get("nonce")
+	if !checkSignature(config.WxToken, signature, timestamp, nonce) {
+		fmt.Fprintf(w, "Invalid signature")
+		return
+	}
+
 	if r.Method == "GET" {
-		// 微信服务器验证
 		echostr := r.URL.Query().Get("echostr")
-		signature := r.URL.Query().Get("signature")
-		timestamp := r.URL.Query().Get("timestamp")
-		nonce := r.URL.Query().Get("nonce")
-		if checkSignature(config.WxToken, signature, timestamp, nonce) {
-			fmt.Fprintf(w, echostr)
-			return
-		} else {
-			fmt.Fprintf(w, "Invalid signature")
-			return
-		}
+		fmt.Fprintf(w, echostr)
+		return
 	} else if r.Method == "POST" {
 		// 接收消息并回复
 		body, err := io.ReadAll(r.Body)
@@ -61,6 +63,15 @@ func checkSignature(token, signature, timestamp, nonce string) bool {
 	sha1Str := sha1.New()
 	_, _ = sha1Str.Write([]byte(strings.Join(strs, "")))
 	encryptedStr := hex.EncodeToString(sha1Str.Sum(nil))
+
+	// 加入时间检测
+	atoi, err := strconv.ParseInt(timestamp, 10, 64)
+	if err != nil {
+		return false
+	}
+	if time.Now().Unix()-atoi > 60 {
+		return false
+	}
 
 	return encryptedStr == signature
 }
