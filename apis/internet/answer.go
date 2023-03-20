7bef379a53e5a32f94515507f41fbd93ab970a3d
yiuked <dev@example.com>
2023-03-20 22:15:17 +0800

    fix:修正配置文件，添加mongo docker

--- apis/internet/answer.go
+++ apis/internet/answer.go
@@ -23,10 +23,20 @@ func GetAnswer(ctx *MsgContext) {
 	if err := config.Mgo.Db.Collection("faq").FindOne(context.Background(), bson.M{"_id": hex}).
 		Decode(&existsFaq); err == nil {
 		if existsFaq.UserName == config.WXOpenId {
-			ResponseText(ctx.ResponseWriter,
-				fmt.Sprintf(`<script src="/md-page.js"></script><noscript>%s<a href="/addDraft?uuid=%s">发到草稿</a>`,
-					existsFaq.Answer, existsFaq.Uuid.Hex()),
-			)
+			if len(config.WxSecret) > 0 &&
+				len(config.WxAppid) > 0 &&
+				len(config.WXOpenId) > 0 &&
+				len(config.WxMediaId) > 0 {
+				ResponseText(ctx.ResponseWriter,
+					fmt.Sprintf(`<script src="/md-page.js"></script><noscript>%s<a href="/addDraft?uuid=%s">发到草稿</a>`,
+						existsFaq.Answer, existsFaq.Uuid.Hex()),
+				)
+			} else {
+				ResponseText(ctx.ResponseWriter,
+					fmt.Sprintf(`<script src="/md-page.js"></script><noscript>%s`, existsFaq.Answer),
+				)
+			}
+
 		} else {
 			ResponseText(ctx.ResponseWriter, fmt.Sprintf(`<script src="/md-page.js"></script><noscript>%s`, existsFaq.Answer))
 		}
